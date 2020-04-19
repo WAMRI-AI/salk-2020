e01b7432a7af33b2758954ba4fd4d02691606860
RusLin-oir <ruslwalk@gmail.com>
2020-04-18 17:13:32 -0700

    TODO: set up training pipeline

--- lab/self-supervised/jigsaw/JigsawNetwork.py
+++ lab/self-supervised/jigsaw/JigsawNetwork.py
@@ -1,17 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 13 15:57:01 2017
-
-@author: Biagio Brattoli
-"""
 import torch
 import torch.nn as nn
 from torch import cat
 import torch.nn.init as init
-
-import sys
-sys.path.append('Utils')
-from Layers import LRN
 
 class Network(nn.Module):
 
@@ -19,7 +9,7 @@ class Network(nn.Module):
         super(Network, self).__init__()
 
         self.conv = nn.Sequential()
-        self.conv.add_module('conv1_s1',nn.Conv2d(3, 96, kernel_size=11, stride=2, padding=0))
+        self.conv.add_module('conv1_s1',nn.Conv2d(1, 96, kernel_size=11, stride=2, padding=0))
         self.conv.add_module('relu1_s1',nn.ReLU(inplace=True))
         self.conv.add_module('pool1_s1',nn.MaxPool2d(kernel_size=3, stride=2))
         self.conv.add_module('lrn1_s1',LRN(local_size=5, alpha=0.0001, beta=0.75))
@@ -87,4 +77,29 @@ def weights_init(model):
     if type(model) in [nn.Conv2d,nn.Linear]:
         nn.init.xavier_normal(model.weight.data)
         nn.init.constant(model.bias.data, 0.1)
-    
+
+class LRN(nn.Module):
+    def __init__(self, local_size=1, alpha=1.0, beta=0.75, ACROSS_CHANNELS=True):
+        super(LRN, self).__init__()
+        self.ACROSS_CHANNELS = ACROSS_CHANNELS
+        if ACROSS_CHANNELS:
+            self.average=nn.AvgPool3d(kernel_size=(local_size, 1, 1),
+                    stride=1,padding=(int((local_size-1.0)/2), 0, 0))
+        else:
+            self.average=nn.AvgPool2d(kernel_size=local_size,
+                    stride=1,padding=int((local_size-1.0)/2))
+        self.alpha = alpha
+        self.beta = beta
+
+
+    def forward(self, x):
+        if self.ACROSS_CHANNELS:
+            div = x.pow(2).unsqueeze(1)
+            div = self.average(div).squeeze(1)
+            div = div.mul(self.alpha).add(1.0).pow(self.beta)
+        else:
+            div = x.pow(2)
+            div = self.average(div)
+            div = div.mul(self.alpha).add(1.0).pow(self.beta)
+        x = x.div(div)
+        return x
